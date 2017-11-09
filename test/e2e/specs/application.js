d9d93bf7c119cf3113a93f57013b4cbfe2bfb828
martinnaughton <martin.oneachtain@gmail.com>
2017-11-09 09:18:11 +0000

    PL-108: added more e2e tests

--- test/e2e/specs/application.js
+++ test/e2e/specs/application.js
@@ -2,6 +2,7 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
+  const visiblePauseTime = 1000;
   'website up Test': function (browser) {
     // automatically uses dev Server port from /config.index.js
     // default: http://localhost:8080
@@ -24,10 +25,46 @@ module.exports = {
     const devServer = browser.globals.devServerURL
     browser
       .url(devServer)
-      .waitForElementVisible('#profiles', 1000)
-      .waitForElementVisible('#reports', 1000)
-      .waitForElementVisible('#users', 1000)
-      .waitForElementVisible('#settings', 1000)
+      .waitForElementVisible('#profiles', visiblePauseTime)
+      .waitForElementVisible('#reports', visiblePauseTime)
+      .waitForElementVisible('#users', visiblePauseTime)
+      .waitForElementVisible('#settings', visiblePauseTime)
+      .end()
+  },
+  'test profiles panel exist': function (browser) {
+    const devServer = browser.globals.devServerURL
+    browser
+      .url(devServer)
+      .waitForElementVisible('#profiles', visiblePauseTime)
+      .click('#profiles')
+      .waitForElementVisible('#profiles-panel', visiblePauseTime)
+      .end()
+  },
+  'test reports panel exist': function (browser) {
+    const devServer = browser.globals.devServerURL
+    browser
+      .url(devServer)
+      .waitForElementVisible('#reports', visiblePauseTime)
+      .click('#reports')
+      .waitForElementVisible('#reports-panel', visiblePauseTime)
+      .end()
+  },
+  'test users panel exist': function (browser) {
+    const devServer = browser.globals.devServerURL
+    browser
+      .url(devServer)
+      .waitForElementVisible('#users', visiblePauseTime)
+      .click('#users')
+      .waitForElementVisible('#users-panel', visiblePauseTime)
+      .end()
+  },
+  'test settings panel exist': function (browser) {
+    const devServer = browser.globals.devServerURL
+    browser
+      .url(devServer)
+      .waitForElementVisible('#settings', visiblePauseTime)
+      .click('#settings')
+      .waitForElementVisible('#settings-panel', visiblePauseTime)
       .end()
   }
 }
