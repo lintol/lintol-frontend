e9da89b869f5cf968e51ad67bc49da638aa0f4b2
martinnaughton <martin.oneachtain@gmail.com>
2017-11-15 22:24:36 +0000

    PL-108: added processors to the add profile list

--- src/components/profiles/AddProfile.vue
+++ src/components/profiles/AddProfile.vue
@@ -5,16 +5,24 @@
     <div class="formContainer">
       <input id="profileName" class="formItem" placeholder="Name" type="text" />
       <textarea id="profileDescription" class="formItem" rows="4" cols="50" placeholder="Description" />
-      <textarea id="profileScript" class="scriptText formItem" rows="4" cols="50" placeholder="Custom Script" />
+      <div>
+      <p>Choose your Processor<p>
+      <p>Instructions<p>
+      <div class="processorContainer">
+        <processor :key="processor.title" :title="processor.title" :description="processor.description" :moreInfo="processor.moreInfo" :type="processor.type" v-for="processor in availableProcessors" v-on:processorSelected="updateProcessors" />
+      </div>
+      </div>
       <div>
         <button id="addProfile" class="addProfileButton" @click=addProfile>Add Profile</button>
       </div>
+      
     </div>
   </div>
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from 'axios';
+import Processor from './Processor';
 export default {
   name: 'Name',
   props: {
@@ -24,7 +32,9 @@ export default {
       title: 'Add Profiles',
       name: '',
       description: '',
-      script: ''
+      script: '',
+      availableProcessors: [],
+      choosenProcessors: []
     };
   },
   methods: {
@@ -34,19 +44,43 @@ export default {
       profile.name = this.name;
       profile.description = this.description;
       profile.script = this.script;
+      profile.processors = this.choosenProcessors;
       axios.put(url, profile).then((response) => {
         this.$router.push({name: 'profiles'});
       }).catch(function (error) {
         console.log('Error adding profile:' + error);
       }); */
       this.$router.push({name: 'profileTable'});
+    },
+    getProcessor: function () {
+      var url = this.$apiPrefix + '/processors/';
+      axios(url).then((response) => {
+        this.availableProcessors = response.data;
+        console.log(this.processors);
+      }).catch(function (error) {
+        console.log('Error adding profile:' + error);
+      });
+    },
+    updateProcessors: function (type, action) {
+      if (action === 'add') {
+        this.choosenProcessors.push(type);
+      } else {
+        var index = this.choosenProcessors.indexOf(type);
+        console.log(index);
+        if (index !== -1) {
+          this.choosenProcessors.splice(index, 1);
+        }
+      }
+      console.log(this.choosenProcessors);
     }
   },
   components: {
+    Processor: Processor
   },
   computed: {
   },
   mounted: function () {
+    this.getProcessor();
   }
 };
 </script>
@@ -54,7 +88,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import '../../assets/scss/application.scss';
-
 .instructions {
 }
 
@@ -65,9 +98,15 @@ export default {
 
 .formContainer {
   display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
+  flex-direction: row;
+  flex-wrap: wrap;
   width: 50%;
+}
+
+.processorContainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
  
 .formItem {
---
+++ src/components/profiles/Processor.vue
@@ -0,0 +1,98 @@
+<template>
+   <div class="processor">
+        <div class="item1">
+          <label></></label>
+          <input id="select" type="checkbox" class="roundCheckbox" v-model=checkboxValue />
+        </div>
+        <label id="processorTitle" class="processorTitle item1">{{ title }}</label>
+        <p id="processorDescription" class="processorDescription item2" >{{ description }}</p>
+        <a id="processorMoreInfo" class="moreInfo item1" href="https://www.w3schools.com" >More Info</a>
+      </div>
+      </div>
+</template>
+
+<script>
+// import axios from 'axios';
+export default {
+  name: 'Name',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    moreInfo: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      checkboxValue: false
+    };
+  },
+  methods: {
+  },
+  components: {
+  },
+  computed: {
+  },
+  watch: {
+    checkboxValue: function () {
+      console.log('checbox changed:' + this.checkboxValue);
+      if (this.checkboxValue) {
+        this.$emit('processorSelected', this.type, 'add');
+      } else {
+        this.$emit('processorSelected', this.type, 'remove');
+      }
+    }
+  },
+  mounted: function () {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '../../assets/scss/application.scss';
+
+.item2 {
+  flex-basis: 2;
+}
+
+.item1 {
+  flex-basis: 1;
+}
+
+.processorTitle {
+  font-weight: bold;
+}
+
+.moreInfo {
+  color: orange;
+  margin-top: auto;
+}
+
+.processor {
+  font-size: 8pt;
+  width: 175px;
+  height: 120px;
+  border: solid 1px black;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  margin: 3px; 
+}
+
+.roundCheckbox {
+  float: right;  
+}
+</style>
