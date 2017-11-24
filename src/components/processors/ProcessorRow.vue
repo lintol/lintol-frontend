1ea1e16e7e7a36a4aa4d2f0c5d526f6c080f0363
martinnaughton <martin.oneachtain@gmail.com>
2017-11-24 19:05:50 +0000

    PL-211: added basic table for processors

---
+++ src/components/processors/ProcessorRow.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="processors-row" class="shadedRow">
+    <div class="processorRow">
+      <div class="processorColumn center">
+        <label>{ processor.name }}</label>
+      </div>
+      <div class="processorColumn center">
+        <p>{{ convertDate(processor.created_at) }}</p>
+      </div>
+      <div class="processorColumn center">
+        <p>{{ convertDate(processor.updated_at) }}</p>
+      </div>
+      <div class="processorColumn center">
+        <p>{{ processor.uniqueTag }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+export default {
+  name: 'ProcessorRow',
+  props: {
+    processor: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+    };
+  },
+  methods: {
+    convertDate: function (dateString) {
+      return moment(dateString).format('Do MMMM YYYY');
+    }
+  },
+  components: {
+  },
+  computed: {
+  },
+  mounted: function () {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import '../../assets/scss/application.scss';
+
+.processorColumn {
+  min-width: 150px;
+  flex: 1;
+}
+ 
+.center {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.processorMainColumn {
+  flex: 2;
+  min-height: 80px;
+  padding: 10px;
+}
+
+.processorAction {
+  min-width: 120px;
+  display: inline-block;
+}
+.processorRow {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+}
+ 
+.columnTitle {
+  font-weight: bold;
+}
+
+.rightSeparator {
+  border-right: grey 1px solid;
+}
+
+.shadedRow {
+  border: 1px solid $rowBackground;
+  border-radius: 3px;
+  background-color: $rowBackground;
+  margin: 20px;
+}
+
+.verticalAlign {
+  vertical-align: middle;
+}
+
+div .processorColumn p {
+  margin: unset;
+  color: grey;
+}
+
+.processorPicture {
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+}
+
+</style>
---
+++ src/components/processors/ProcessorTable.vue
@@ -0,0 +1,71 @@
+<template>
+   <div id="processorTablePanel">
+    <div >
+      <button id="addNewProcessorButton" class="addProcessorButton" @click="addProcessor">Add new Data Processor +</button>
+      <h1>{{ title }}</h1>
+      <p class="instructions">
+        Instructions
+      </p>
+      <input id="processorSearch" type="text" v-model="search">
+      <!-- tables -->
+      <div id="headings"  class="headerContainer tableSeparator">
+        <label class="flexHeading">Name</label> 
+        <label class="flexHeading">Description</label> 
+        <label class="flexHeading">Creator</label> 
+        <label class="flexHeading">Created</label> 
+        <label class="flexHeading">Last Updated</label> 
+        <label class="flexHeading">Unique Tag</label> 
+      </div>
+      <div id="noProcessorsAvailable" v-if="profiles.length == 0">
+        <p class="instructions"> No Processors available for this account</p>
+      </div>
+      <div id="columns"  v-else class="flexContainer">
+        <processor-row :key="processor.name" v-for="processor in processors" :processor="processor"></processor-row>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import ProcessorRow from 'ProcessorRow';
+export default {
+  name: 'ProcessorTable',
+  props: {
+  },
+  data () {
+    return {
+      title: 'Processors',
+      search:'',
+      processors: []
+    };
+  },
+  methods: {
+    getProcessors: function () {
+      axios.get(this.$apiPrefix + '/processors/').then(response => {
+        this.processors = response.data;
+        console.log(response.data);
+      }, response => {
+        console.log('Couldnt get processor.');
+      });
+    },
+    addProcessor: function () {
+      this.$router.push({name: 'addProcessor'});
+    }
+  },
+  components: {
+  },
+  computed: {
+  },
+  mounted: function () {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import '../../assets/scss/application.scss';
+
+. {
+}
+</style>
