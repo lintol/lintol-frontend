1dc4e9ccadf56a99a3ba4a8dac49c9bd97716719
martinnaughton <martin.oneachtain@gmail.com>
2017-11-29 21:10:07 +0000

    Turning off automatic browser popup

--- build/dev-server.js
+++ build/dev-server.js
@@ -94,7 +94,7 @@ devMiddleware.waitUntilValid(() => {
     console.log('> Listening at ' + uri + '\n')
     // when env is testing, don't need open it
     if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-      opn(uri)
+//      opn(uri)
     }
     server = app.listen(port)
     _resolve()
