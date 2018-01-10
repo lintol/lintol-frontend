9c73d2279a5bb31399de73dea2d35a7100d59365
martinnaughton <martin.oneachtain@gmail.com>
2018-01-10 16:10:46 +0000

    Fixed marker issue in Vue2Leaflet

--- src/main.js
+++ src/main.js
@@ -7,6 +7,17 @@ import VeeValidate from 'vee-validate';
 import store from './state/store';
 import Vue2Leaflet from 'vue2-leaflet';
 
+/* Work around to get Vue2Leaflet markers working */
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+/**************************************/
+
 Vue.config.productionTip = false;
 Vue.use(VeeValidate);
 
