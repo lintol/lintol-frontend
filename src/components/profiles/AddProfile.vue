f1acb92675cf3347e802e8f9c1fb516a5fd99505
Phil Weir <phil.weir@flaxandteal.co.uk>
2018-02-11 05:25:16 +0000

    pull boundary for map from supplementary data

--- src/components/profiles/AddProfile.vue
+++ src/components/profiles/AddProfile.vue
@@ -51,7 +51,6 @@ export default {
   methods: {
     addProfile: function () {
       this.$validator.validateAll().then((result) => {
-        this.profile.configurations = this.chosenProcessors;
         this.$store.dispatch(STORE_PROFILE, {
           profile: this.profile,
           configurations: this.chosenProcessors
