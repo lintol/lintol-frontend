c9d014900fd3089d2d58a053e04c8c8528a71661
martinnaughton <martin.oneachtain@gmail.com>
2019-03-13 19:58:46 +0000

    Fixed issue with profile rows in UI spacing

--- src/components/profiles/ProfileRow.vue
+++ src/components/profiles/ProfileRow.vue
@@ -1,31 +1,39 @@
 <template>
   <b-container>
-    <b-row class="profileRow shadedRow fullSize">
-      <b-col cols="3" class="profileColumn profileMainColumn center first fullSize">
+    <b-row class="shadedRow" no-gutters>
+      <b-col cols="3" class="profileMainColumn center">
         <div class="rightSeparator">
           <label class="profileName"> {{ profile.name }} </label>
           <user v-if="profile.creator" class="user" :user="profile.creator"></user>
           <p class="description" >{{ profile.description }} </p>
         </div>
       </b-col>
-      <b-col cols="2" class="profileColumn center third fullSize">
+      <b-col cols="1" class="profileColumn center">
+        <div>
         <label class="columnTitle">Created</label>
         <p>{{ convertDate(profile.created_at.date) }}</p>
+        </div>
       </b-col>
-      <b-col cols="2" class="profileColumn center third fullSize">
+      <b-col cols="2" class="profileColumn center">
+        <div>
         <label class="columnTitle">Last Updated</label>
         <p>{{ convertDate(profile.updated_at.date) }}</p>
+        </div>
       </b-col>
-      <b-col cols="2" class="profileColumn center second fullSize">
-        <label class="columnTitle">Version</label>
-        <p >{{ profile.version }}</p>
+      <b-col cols="2" class="profileColumn center">
+        <div>
+          <label class="columnTitle">Version</label>
+          <p>{{ profile.version }}</p>
+        </div>
       </b-col>
-      <b-col cols="2" class="profileColumn center third fullSize" >
+      <b-col cols="2" class="profileColumn center">
+        <div>
         <label class="columnTitle">Unique Tag</label>
         <p>{{ profile.uniqueTag }}</p>
+        </div>
       </b-col>
-      <b-col cols="2" class="profileColumn verticalCenterItems third ">
-        <select class="fullSize profileActions verticalAlign viewAction" id="profileAction" @click=profileAction>
+      <b-col cols="2" class="profileColumn verticalCenterItems">
+        <select class="profileActions verticalAlign viewAction" id="profileAction" @click=profileAction>
           <option style='text-align: left' value="">Edit/Archive</option>
           <option value="edit">Edit</option>
           <option value="archive">Archive</option>
@@ -83,12 +91,15 @@ label {
 }
 
 .profileColumn {
-
+ > div {
+   margin: auto;
   > label {
     font-weight: bold;
     font-size: 0.625em;
   }
+ }
 }
+
 .center {
   display: flex;
   flex-direction: column;
@@ -97,14 +108,16 @@ label {
 
 .profileMainColumn {
   padding: 0.625em;
+  p {
+  margin-top: 4px;
+  color: grey;
+  font-size: 0.6875em;
+}
 }
 
 .profileAction {
   min-width: 120px;
   display: inline-block;
-}
-.profileRow {
-  justify-content: space-between;
 }
 
 .verticalAlign {
@@ -145,58 +158,7 @@ div .profileColumn p {
   color: #777776;
 }
 
-.first{
-  order: 0;
-}
-
-.second{
-  order : 0;
-}
-
-.third{
-  order: 0;
-}
-
-.bottom-bar{
-  border-bottom:1px solid #E5E5E5;
-  margin-bottom: 15px;
-}
-
-.open{
-  display: flex;
-}
-.show{
-  display: none;
-}
 .rightSeparator{
   padding-right: 5px;
-}
-@media (max-width: 376px){
-  .first{
-    order: 1;
-    padding: 10px 0px;
-  }
-
-  .second{
-    order : 2;
-  }
-
-  .third{
-    order: 3;
-    padding: 5px 10px 5px 10px;
-  }
-
-  .show{
-    display: flex;
-  }
-
-  .open{
-    display: none;
-  }
-  .fullSize{
-    width:100%;
-    text-align: center;
-    text-align-last: center;
-  }
 }
 </style>
